a3c54d176e19907c3e91daa879f0b9ea9f70106b
HemantPatilQA <hemantkpatil@yahoo.com>
2022-03-08 22:20:47 -0800

    PostMapping implementation with Service Layer

--- src/main/java/com/self/mslearning/controller/MyUserController.java
+++ src/main/java/com/self/mslearning/controller/MyUserController.java
@@ -24,24 +24,15 @@ public class MyUserController {
 
     @PostMapping(value = "/userManager/addUser", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<List<MyUser>> addUser(@RequestBody MyUser myUser){
-        return new ResponseEntity<List<MyUser>>(myUserMgr.addUser(myUser), HttpStatus.OK);
-    }
-/*
-    @GetMapping(value = "/userManager/getUser", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<List<MyUser>> getUsers(@RequestParam(value = "name", required = false) String sName){
-        if(sName != null) {
-            List<MyUser> myLocalUserList = new ArrayList<MyUser>();
-            myUserList.forEach(myUser -> {
-                if (myUser.getfName().contains(sName) || myUser.getlName().contains(sName) || myUser.getUserName().contains(sName))
-                    myLocalUserList.add(myUser);
-            });
-            return new ResponseEntity<List<MyUser>>(myLocalUserList, HttpStatus.OK);
-        }
-        else
-            return new ResponseEntity<List<MyUser>>(myUserList, HttpStatus.OK);
+        return new ResponseEntity<List<MyUser>>(myUserMgr.addUser(myUser), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/userManager/deleteUser")
+    @GetMapping(value = "/userManager/getUser", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<List<MyUser>> getUsers(@RequestParam(value = "name", required = false) String sName){
+        return new ResponseEntity<List<MyUser>>(myUserMgr.getUsers(sName), HttpStatus.OK);
+    }
+
+/*    @DeleteMapping("/userManager/deleteUser")
     public @ResponseBody ResponseEntity<List<MyUser>> removeUser(@RequestParam(value = "name", required = false) String sName){
         if(sName != null) {
             List<MyUser> myLocalUserList = new ArrayList<MyUser>();
